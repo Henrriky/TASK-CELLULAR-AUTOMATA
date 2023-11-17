b2e5ec9a5bc38d53df1969278a3c1247a50b80b0
Recr <126016150+Recr@users.noreply.github.com>
2023-11-17 17:27:10 -0300

    primeiro commit

---
+++ src/celularautomaton/Main.java
@@ -0,0 +1,14 @@
+package celularautomaton;
+
+public class Main {
+	public static void main(String[] args) {
+		World a = new World(200);
+//		System.out.println(a);
+//		a.a();
+//		System.out.println(a);
+//		double i = 0.995;
+//		200*200*suscetibleRate
+//		System.out.println((int)(200*200*i)); 
+	}
+
+}
