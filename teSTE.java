c26c812c0fc3f5d64fb0f31111317dc342455d5c
Henrriky <tectutors.123@gmail.com>
2023-11-19 14:29:42 -0300

    removendo arquivo de teste

--- teSTE.java
+++
@@ -1,9 +0,0 @@
-package WORK;
-
-public class teSTE {
-	public static void main(String[] args) {
-		States grafo = new States();
-		//System.out.println(grafo.getEdgesFromVertex(StatePossibles.INFECTADO.getStateName()));
-		System.out.println(grafo.getEdgesFromVertex(StatePossibles.RECUPERADO.getStateName()));
-	}
-}
